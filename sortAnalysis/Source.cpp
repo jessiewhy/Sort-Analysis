f4ffcc81f56c0d46476007e5f37769a0004394ba
Jessie Huang <jessiemh@ufl.edu>
2018-07-13 11:09:27 -0400

    7.13.2018: implementation done

--- sortAnalysis/Source.cpp
+++ sortAnalysis/Source.cpp
@@ -30,12 +30,6 @@ void bubbleSort(std::vector<int> &sortThisVector, int size) {
 
 }
 
-void print(std::vector<int> myVec) {
-	std::vector<int>::iterator pos;
-	for (pos = myVec.begin(); pos != myVec.end(); ++pos)
-		std::cout << *pos << " " << std::endl;
-}
-
 void heapify(std::vector<int> &sortThisVector, int size, int calcParent) {
 	int largest = calcParent;
 	int lIndex = 2 * calcParent + 1;
@@ -53,7 +47,6 @@ void heapify(std::vector<int> &sortThisVector, int size, int calcParent) {
 }
 
 void heapSort(std::vector<int> &sortThisVector, int size) {
-	std::string sortType = "Heap Sort";
 	auto start = high_resolution_clock::now();
 	for (int i = size / 2 - 1; i >= 0; i--) {
 		heapify(sortThisVector, size, i);
@@ -69,6 +62,77 @@ void heapSort(std::vector<int> &sortThisVector, int size) {
 	std::cout << "Time taken for heap sort: " << duration.count() << " seconds" << std::endl;
 	
 	
+}
+
+void merge(std::vector<int> &sortThisVector, int low, int mid, int high) {
+	int i, j, k;
+	int n1 = mid - low + 1;
+	int n2 = high - mid;
+
+	std::vector<int> left(n1); 
+	std::vector<int> right(n2);
+
+	for (i = 0; i < n1; i++)
+		left[i] = sortThisVector[low + i];
+	for (j = 0; j < n2; j++)
+		right[j] = sortThisVector[mid + 1 + j];
+
+	i = 0;
+	j = 0;
+	k = low;
+	while (i < n1 && j < n2)
+	{
+		if (left[i] <= right[j])
+		{
+			sortThisVector[k] = left[i];
+			i++;
+		}
+		else
+		{
+			sortThisVector[k] = right[j];
+			j++;
+		}
+		k++;
+	}
+
+	while (i < n1)
+	{
+		sortThisVector[k] = left[i];
+		i++;
+		k++;
+	}
+
+	while (j < n2)
+	{
+		sortThisVector[k] = right[j];
+		j++;
+		k++;
+	}
+}
+
+void mergeSort(std::vector<int> &sortThisVector, int low, int high) {
+	auto start = high_resolution_clock::now();
+	int mid;
+	if (low < high) {
+		mid = low + (high-low) / 2;
+		mergeSort(sortThisVector, low, mid);
+		mergeSort(sortThisVector, mid + 1, high);
+
+		merge(sortThisVector, low, mid, high);
+	}
+	else {
+		auto stop = high_resolution_clock::now();
+		auto duration = duration_cast<seconds>(stop - start);
+		std::cout << "Time taken for merge sort: " << duration.count() << " seconds" << std::endl;
+	}
+
+	//std::cout << "test" << std::endl;
+}
+
+void print(std::vector<int> myVec) {
+	std::vector<int>::iterator pos;
+	for (pos = myVec.begin(); pos != myVec.end(); ++pos)
+		std::cout << *pos << " " << std::endl;
 }
 
 //DON'T FORGET TO CLOSE FILE WHEN YOU'RE DONE IMPLEMENTING SORTING METHODS 
@@ -103,9 +167,10 @@ int main()
 	//sort vectors
 	//bubbleSort(myVector, count);
 	//print(myVector);
-	heapSort(myVector, count);
+	//heapSort(myVector, count);
 	//print(myVector);
-	//myVector.mergeSort();
+	mergeSort(myVector, 0, count - 1);
+	//print(myVector);
 	system("pause");
 
 }
